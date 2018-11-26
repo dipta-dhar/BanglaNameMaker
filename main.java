48d8e49d0fb88e9155d7a0758c1a6df73cda654d
Dipta Dip <30615936+dipta1010@users.noreply.github.com>
2018-11-26 19:34:51 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,15 @@
 // Main Class
+
+
+public class NameGenerator {
+
+  @Override
+  public String nextName() {
+    return "";
+  }
+
+  @Override
+  public String getName(long seed) {
+    return "";
+  }
+}
