1a54fae1194259b21c61a24f2edf9857289f0f98
Dipta Dip <30615936+dipta1010@users.noreply.github.com>
2019-03-11 13:38:25 +0600

    banglaNameMaker

---
+++ src/Index/INdex.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Index;
+
+import View.MScreen;
+
+/**
+ *
+ * @author dipta
+ */
+public class INdex {
+    public static void main(String[] args) {
+        MScreen ms = new MScreen();
+        ms.setVisible(true);
+    }
+}
